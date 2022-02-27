b50287b19e3c8305e399338bfe4505d3d5c35c65
Blake Georgii <blakegeorgii@gmail.com>
2022-02-27 12:20:21 -0800

    set up associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.BelongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.BelongsToMany(Tag);
 
 // Tags belongToMany Products (through ProductTag)
+Tag.BelongsToMany(Product);
 
 module.exports = {
   Product,
