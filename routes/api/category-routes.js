2286a7baffaf97ee5c1a42b285e5bfb03cd71571
Blake Georgii <blakegeorgii@gmail.com>
2022-02-27 15:15:07 -0800

    added category get by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,16 +27,30 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    // include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
